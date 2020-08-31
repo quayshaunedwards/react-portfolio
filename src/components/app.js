2efb5ae6d4d4f6d28cb8c0b2d7d6d49e7b5175f3
quayshaunedwards <qu.edwards1@gmail.com>
2020-08-31 17:20:17 -0500

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import moment from "moment";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>React Portfolio</h1>
+    <div>{moment().format('MMM Do YYYY, h:mm:ss a')}</div>
+      </div>
+    );
+  }
+}
